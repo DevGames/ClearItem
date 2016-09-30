da72a9ae418ca04827ec9cf303642611f65e8ae4
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-09-30 16:01:50 -0700

    Update Task.php

--- src/ItemClear/Task.php
+++ src/ItemClear/Task.php
@@ -6,14 +6,14 @@ class Task extends \pocketmine\scheduler\PluginTask{
         
     public $owner;
 
-    public function __construct(API $owner) {
+    public function __construct(Main $owner) {
     $this->owner = $owner;
     }
     
     public function onRun($tick){
-       foreach($this->owner->getVar()->config->get("Worlds") as $worldconfig){foreach($this->owner->getVar()->config->get("Item") as $items){
+       foreach($this->owner->config->get("Worlds") as $worldconfig){foreach($this->owner->config->get("Item") as $items){
         foreach ($this->owner->getServer()->getOnlinePlayers() as $p){
-        $a = $this->owner->getPlayer($this->owner->getVar()->array, $p);
+        $a = $this->owner->getPlayer($this->owner->array, $p);
            if($worldconfig == $this->owner->array["w"][$a->getName()]){
                 $a->getInventory()->removeItem(\pocketmine\item\Item::get($items,0,PHP_INT_MAX));
            }
