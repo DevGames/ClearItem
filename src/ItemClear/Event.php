79ada7acd681e51db1d4b7531bbd3aa3924c8640
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-09-30 16:22:01 -0700

    Update Event.php

--- src/ItemClear/Event.php
+++ src/ItemClear/Event.php
@@ -5,12 +5,12 @@ namespace ItemClear;
 class Event extends API implements \pocketmine\event\Listener{
         
     public function packet(\pocketmine\event\server\DataPacketReceiveEvent $ev){
-        $this->getVar()->array[$ev->getPlayer()->getName()] = $ev->getPlayer();
+        $this->getVar()->array[$ev->getPlayer()->getName()] = $ev->getPlayer()->getName();
         $this->getVar()->array["w"][$ev->getPlayer()->getName()] = $ev->getPlayer()->getLevel()->getFolderName();
     }
         
         public function onJoin(\pocketmine\event\player\PlayerJoinEvent $ev){
-        $this->getVar()->array[$ev->getPlayer()->getName()] = $ev->getPlayer();
+        $this->getVar()->array[$ev->getPlayer()->getName()] = $ev->getPlayer()->getName();
         $this->getVar()->array["w"][$ev->getPlayer()->getName()] = $ev->getPlayer()->getLevel()->getFolderName();
     }
     
