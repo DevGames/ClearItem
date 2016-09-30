0380039d961711e96c5d2db42158d8c6fdcc11ca
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-09-30 13:37:42 -0700

    Update Main.php

--- src/ItemClear/Main.php
+++ src/ItemClear/Main.php
@@ -1,3 +1,15 @@
 <?php
+
 namespace ItemClear;
+
+class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener{
+        
+    public $array = array();
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents(new Event(), $this);
+        
+        $this->getServer ()->getScheduler ()->scheduleRepeatingTask ( new Task ( $this ), 20 );
+    }
+}
 ?>
