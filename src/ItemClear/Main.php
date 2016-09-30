537c32b13e5fcd76e98df98c5abf57f27638351b
Khaled_Craft1b <iiBa4x@gmail.com>
2016-10-01 01:34:42 +0300

    Update Main.php

--- src/ItemClear/Main.php
+++ src/ItemClear/Main.php
@@ -5,7 +5,6 @@ class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Li
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents(new Event(), $this);
         $this->getServer()->getScheduler()->scheduleRepeatingTask(new Task($this), 20);
-        $this->getServer()->getScheduler()->scheduleRepeatingTask(new TaskItem($this), 20);
         $cfg = [
             "Worlds" => array("World","World"),
             "Item" => array(0,0),
