4f292d1a98ae822cb365780fbc2d8a390570d5bf
Khaled_Craft1b <iiBa4x@gmail.com>
2016-09-30 23:44:46 +0300

    Update Main.php

--- src/ItemClear/Main.php
+++ src/ItemClear/Main.php
@@ -1,15 +1,10 @@
 <?php
-
 namespace ItemClear;
-
 class Main extends \pocketmine\plugin\PluginBase implements \pocketmine\event\Listener{
-        
     public $array = array();
-
     public function onEnable() {
         $this->getServer()->getPluginManager()->registerEvents(new Event(), $this);
-        
-        $this->getServer ()->getScheduler ()->scheduleRepeatingTask ( new Task ( $this ), 20 );
+        $this->getServer ()->getScheduler ()->scheduleRepeatingTask (new Task ($this), 20);
     }
 }
 ?>
