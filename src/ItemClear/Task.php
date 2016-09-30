346f889a5459dcc4b6911dd9d6a002f357572b6d
Nawaf_Craft1b <Nawafmg97@gmail.com>
2016-09-30 13:45:13 -0700

    Create Task.php

---
+++ src/ItemClear/Task.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ItemClear;
+
+class Task extends \pocketmine\scheduler\PluginTask{
+        
+    public $owner;
+
+    public function __construct(API $owner) {
+    $this->owner = $owner;
+    }
+    
+    public function onRun($tick){
+        foreach ($this->owner->getServer()->getOnlinePlayers() as $p){
+        $a = $this->owner->getPlayer($this->owner->getVar()->array, $p);
+        foreach ($a->getInventory()->getContents() as $inv){
+            if($inv->getId() == $item){
+                $a->getInventory()->removeItem(\pocketmine\item\Item::get($item,0,PHP_INT_MAX));
+            }
+        }
+    }
+  }
+}
+?>
