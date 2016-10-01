f9ad9aabf3fa8aafd5909493245f26369e8c3788
Khaled_Craft1b <iiBa4x@gmail.com>
2016-10-01 14:55:42 +0300

    Create Task.php

---
+++ src/ClearItem/Task.php
@@ -0,0 +1,20 @@
+<?php
+namespace ClearItem;
+class Task extends \pocketmine\scheduler\PluginTask{
+    public $owner;
+    public function __construct(Main $owner){
+    $this->owner = $owner;
+    }
+    public function onRun($tick){
+        foreach($this->owner->config->get("Worlds") as $worldconfig){
+            foreach($this->owner->config->get("Item") as $items){
+                foreach($this->owner->getServer()->getOnlinePlayers() as $player){
+                    if($worldconfig == $player->level->getName()){
+                        $player->getInventory()->removeItem(\pocketmine\item\Item::get($items,0,PHP_INT_MAX));
+                    }
+                }
+            }
+        }
+    }
+}
+?>
