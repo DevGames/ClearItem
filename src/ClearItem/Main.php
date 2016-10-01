f5449a6a19f2bfd7b65e848036a4c957d2791bed
Khaled_Craft1b <iiBa4x@gmail.com>
2016-10-01 16:36:01 +0300

    Update Main.php

--- src/ClearItem/Main.php
+++ src/ClearItem/Main.php
@@ -3,6 +3,7 @@ namespace ClearItem;
 class Main extends \pocketmine\plugin\PluginBase{
     public $config;
     public function onEnable(){
+        @mkdir($this->getDataFolder());
         $cfg = ["Worlds" => array("World","World"),"Item" => array(0,0),];
         $this->getServer()->getScheduler()->scheduleRepeatingTask(new Task($this), 20);
         $this->config = new \pocketmine\utils\Config($this->getDataFolder()."config.yml",\pocketmine\utils\Config::YAML,$cfg);
